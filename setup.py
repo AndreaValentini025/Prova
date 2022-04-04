d6d62cde8c7bd6f27ecf03fbc8abaa59242d59df
andre <andreavalentini2025@gmail.com>
2022-04-04 10:28:11 +0200

    build successfull

--- setup.py
+++ setup.py
@@ -1,3 +1,26 @@
-from setuptools import setup
+import setuptools
 
-setup()
+with open("README.rst", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="polls-AndreVale025",
+    version="0.0.1",
+    author="Example Author",
+    author_email="author@example.com",
+    description="A small example package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/AndreaValentini025/",
+    project_urls={
+        "Polls": "https://github.com/AndreaValentini025/Prova.git",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "django-polls"},
+    packages=setuptools.find_packages(where="django-polls"),
+    python_requires=">=3.6",
+)
