e4be27ab0bf3381ac27d9be36595f2326b8e7102
andre <andreavalentini2025@gmail.com>
2022-04-04 11:36:45 +0200

    build successfull

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.rst", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="polls-AndreVale025",
-    version="0.0.2",
+    version="0.0.3",
     author="Example Author",
     author_email="author@example.com",
     description="A small example package",
