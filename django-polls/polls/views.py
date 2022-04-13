be9246d474d4693fde50a1cd70eddbd5007a8bd0
andre <andreavalentini2025@gmail.com>
2022-04-13 18:11:25 +0200

    added provaPage.html

--- django-polls/polls/views.py
+++ django-polls/polls/views.py
@@ -50,3 +50,7 @@ def vote(request, question_id):
         # with POST data. This prevents data from being posted twice if a
         # user hits the Back button.
         return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+
+
+def modulo(request):
+    return render(request, 'polls/paginaProva.html')
